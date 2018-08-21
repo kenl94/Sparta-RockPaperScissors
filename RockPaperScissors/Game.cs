915a7fc1adf3c172ea2abfe1a4f89a7af1534080
kenl94 <kly@spartaglobal.com>
2018-08-21 09:53:39 +0100

    RockPaperScissors game works, need to fix loop

---
+++ RockPaperScissors/Game.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors
+{
+    class Game
+    {
+        Random random = new Random();
+        string rPS;
+        int computerMove;
+        List<int> playerWins;
+        List<int> computerWins;
+
+        public Game()
+        {
+            WelcomeMessage();
+            BestOfFive();
+            
+            
+        }
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Rock Paper Scissors.. \nThis is a best out of 5 \nChoose your move: Rock, Paper, Scissors");
+
+        }
+
+        public void BestOfFive()
+        {
+            playerWins = new List<int>();
+            computerWins = new List<int>();
+            while (playerWins.Count != 5 | computerWins.Count != 5 )
+            {
+                ChooseMove();
+                ComputerGenerateMove();
+                winOrLose();
+            }
+        }
+
+        public void ChooseMove()
+        {
+            rPS = Console.ReadLine().ToUpper();
+        }
+
+
+        public void ComputerGenerateMove()
+        {
+            computerMove = random.Next(1,4);
+            Console.WriteLine(computerMove);
+        }
+
+
+        public void winOrLose()
+        {
+
+            if (rPS == "ROCK")
+            {
+                if (computerMove == 1)
+                {
+                    Console.WriteLine("Draw! Computer picked Rock");
+                }
+                else if (computerMove == 2)
+                {
+                    Console.WriteLine("Winner! Computer picked Scissors");
+                    playerWins.Add(1);
+                    Console.WriteLine(playerWins.Count + "Playerwins");
+                }
+                else if (computerMove == 3)
+                {
+                    Console.WriteLine("Loser! Computer picked Paper");
+                    computerWins.Add(1);
+
+                }
+
+            }
+
+            else if (rPS == "PAPER")
+            {
+                if (computerMove == 1)
+                {
+                    Console.WriteLine("Draw! Computer picked Paper");
+                }
+                else if (computerMove == 2)
+                {
+                    Console.WriteLine("Winner! Computer picked Rock");
+                    playerWins.Add(1);
+
+                }
+                else if (computerMove == 3)
+                {
+                    Console.WriteLine("Loser! Computer picked Scissors");
+                    computerWins.Add(1);
+
+                }
+            }
+
+            else if (rPS == "SCISSORS")
+            {
+                if (computerMove == 1)
+                {
+                    Console.WriteLine("Draw! Computer picked Scissors");
+                }
+                else if (computerMove == 2)
+                {
+                    Console.WriteLine("Winner! Computer picked Paper");
+                    playerWins.Add(1);
+
+                }
+                else if (computerMove == 3)
+                {
+                    Console.WriteLine("Loser! Computer picked Rock");
+                    computerWins.Add(1);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong output!");
+            }
+            Console.WriteLine("Press Enter for the next round");
+            Console.ReadLine();
+            if (playerWins.Count < 5 || computerWins.Count < 5)
+            {
+                Console.WriteLine("Rock, Paper or Scissors?");
+            }
+
+           
+            
+        }
+
+
+    }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,7 +6,7 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game game = new Game();
         }
     }
 }
